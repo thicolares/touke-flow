02a45734a5742cb1b7c469865351db33cf064a35
Thiago Colares <thiago@apimenti.com.br>
2013-09-09 07:41:22 -0300

    Removed migrate task

--- Build/Surf/MyDeployment.php
+++ Build/Surf/MyDeployment.php
@@ -9,6 +9,13 @@
 	$application->setDeploymentPath('/var/www/web/touke/htdocs');
 	$application->setOption('repositoryUrl', 'https://github.com/colares/touke-flow.git');
    $application->setOption('composerCommandPath', '/usr/local/bin/composer');
+   $application->setOption('keepReleases', 2);
+   
+   /**
+    * renenabled it JUST after create a DB setup file
+    */
+   $application->setOption('removeTask', 'migrate');
+
    //   $application->setOption('composerCommandPath', 'php /var/www/vhosts/neos.typo3.org/home/composer.phar');
    
 	$application->addNode($node);
