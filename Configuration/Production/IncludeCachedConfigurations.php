0cef2314f32eb6c8eace2a6fcdf3ff0e065e0169
Thiago Colares <thiago@apimenti.com.br>
2013-09-12 21:29:15 -0300

    Deploy

--- Configuration/Production/IncludeCachedConfigurations.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (FLOW_PATH_ROOT !== '/Library/WebServer/Documents/touke-flow/' || !file_exists('/Library/WebServer/Documents/touke-flow/Data/Temporary/Production/Configuration/ProductionConfigurations.php')) {
-	unlink(__FILE__);
-	return array();
-}
-return require '/Library/WebServer/Documents/touke-flow/Data/Temporary/Production/Configuration/ProductionConfigurations.php';
-?>
