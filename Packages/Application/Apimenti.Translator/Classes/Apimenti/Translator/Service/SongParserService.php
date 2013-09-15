4c2ffaa25f5de92592829111640298ad77e65bcf
Thiago Colares <thiago@apimenti.com.br>
2013-09-15 12:25:37 -0300

    Added the terms of the GNU Affero General Public License

--- Packages/Application/Apimenti.Translator/Classes/Apimenti/Translator/Service/SongParserService.php
+++ Packages/Application/Apimenti.Translator/Classes/Apimenti/Translator/Service/SongParserService.php
@@ -2,6 +2,16 @@
 
 namespace Apimenti\Translator\Service;
 
+/*                                                                            *
+ * This script belongs to the ToUke TYPO3 Flow package "Apimenti.Translator". *
+ *                                                                            *
+ * It is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU Affero General Public License as published by         *
+ * the Free Software Foundation; either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ *                                                                            */
+
 use TYPO3\Flow\Annotations as Flow;
 
 class SongParserService {
